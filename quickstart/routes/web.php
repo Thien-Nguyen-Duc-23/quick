48046748cf1262ea93d30b8b5449fa0ad1791723
Thien-Nguyen-Duc-23 <tducnguyen1997@gmail.com>
2018-06-09 23:51:25 +0700

    update route

--- quickstart/routes/web.php
+++ quickstart/routes/web.php
@@ -10,7 +10,24 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Task;
+use Illuminate\Http\Request;
+Route::get('/a', function () {
+    return view('tasks');
+});
 
-Route::get('/', function () {
-    return view('welcome');
+ Route::get('/','HomeController@getValue') ;
+ 
+ Route::post('/tasks',function(Request $request){
+
+ 	$task = new Task();
+      $task->name = $request->name;
+     $task->save();
+     return redirect('/');
+ });
+
+ Route::delete('/tasks/{task}',function(tasks $task)
+{
+	$task->delete();
+	return redirect('/');
 });
